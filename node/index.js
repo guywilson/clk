e6c3993dc65b905aadf04b73946842820204a303
Guy Wilson <wilson.guy@gmail.com>
2020-02-12 16:16:16 +0000

    Updated test index.js file

--- node/index.js
+++ node/index.js
@@ -6,7 +6,7 @@ console.log('version = ' + version);
 var filelength = cloak.getFilelength('../LICENSE');
 console.log('File length of input file is ' + filelength);
 
-cloak.hide('../flowers.png', '../LICENSE', '/Users/guy/temp/flowers_out.png', 1, 'PNG', 'AES-256', 'password');
-cloak.reveal('/Users/guy/temp/flowers_out.png', '/Users/guy/temp/LICENSE.txt', 1, 'AES-256', false, 'password');
+cloak.hide('../Lenna.png', '../LICENSE', '/Users/guy/temp/Lenna_out.png', 1, 'PNG', 'AES-256', 'password');
+cloak.reveal('/Users/guy/temp/Lenna_out.png', '/Users/guy/temp/LICENSE.txt', 1, 'AES-256', false, 'password');
 
 module.exports = cloak;
