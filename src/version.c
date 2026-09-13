4118f5536fab46dca98113e04f132c0fa5813f6c
Guy Wilson <wilson.guy@gmail.com>
2026-09-13 09:45:27 +0100

    Reset version to 1.0.0 for distribution

--- src/version.c
+++ src/version.c
@@ -1,7 +1,7 @@
 #include "version.h"
 
-#define __BDATE__      "2026-09-13 09:42:43"
-#define __BVERSION__   "1.0.019"
+#define __BDATE__      "2026-09-13 09:44:48"
+#define __BVERSION__   "1.0.000"
 
 const char * getVersion(void) {
     return __BVERSION__;
